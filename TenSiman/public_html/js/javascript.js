48fc7ecb9de6463be71850e2edc8b5ac1c2c6dd8
stav moskovich <smoskovich@vmware.com>
2014-03-26 15:53:05 +0200

    ljclj

--- TenSiman/public_html/js/javascript.js
+++ TenSiman/public_html/js/javascript.js
@@ -777,8 +777,10 @@ function playTurn(game_id) {
                 FB.getLoginStatus(function(response) {
                     if (response.status == 'connected') {
                         alert('logged in!!!');
-                        alert(response.id);
-                        currentPlayerId = response.id;
+                        var uid = response.authResponse.userID;
+                        var accessToken = response.authResponse.accessToken;
+                        alert("id: " + uid);
+                        currentPlayerId = uid;
                         
                         
 //              $.ajax({
