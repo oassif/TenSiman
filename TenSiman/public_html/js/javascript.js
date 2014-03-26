44d88e6ecb9f9da98e480fe7bfc946e594de392d
stav moskovich <smoskovich@vmware.com>
2014-03-26 11:21:05 +0200

    l;jdlk

--- TenSiman/public_html/js/javascript.js
+++ TenSiman/public_html/js/javascript.js
@@ -41,6 +41,30 @@ function resizeWidthIframe() {
 
 $(document).ready(function()
 {
+    
+     if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined'))
+                alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
+            if (typeof CDV == 'undefined')
+                alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+            if (typeof FB == 'undefined')
+                alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+            FB.Event.subscribe('auth.login', function(response) {
+                alert('auth.login event');
+            });
+
+            FB.Event.subscribe('auth.logout', function(response) {
+                alert('auth.logout event');
+            });
+
+            FB.Event.subscribe('auth.sessionChange', function(response) {
+                alert('auth.sessionChange event');
+            });
+
+            FB.Event.subscribe('auth.statusChange', function(response) {
+                alert('auth.statusChange event');
+            });
+            
     refreshMatchups();
 //    videoSource[0] = 'movies/new.gif';
 //    videoSource[1] = 'movies/after.gif';
@@ -709,29 +733,6 @@ function playTurn(game_id) {
 //    refreshMatchups();
 //}
 
- if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined'))
-                alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
-            if (typeof CDV == 'undefined')
-                alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
-            if (typeof FB == 'undefined')
-                alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
-
-            FB.Event.subscribe('auth.login', function(response) {
-                alert('auth.login event');
-            });
-
-            FB.Event.subscribe('auth.logout', function(response) {
-                alert('auth.logout event');
-            });
-
-            FB.Event.subscribe('auth.sessionChange', function(response) {
-                alert('auth.sessionChange event');
-            });
-
-            FB.Event.subscribe('auth.statusChange', function(response) {
-                alert('auth.statusChange event');
-            });
-
             /*function getSession() {
              alert("session: " + JSON.stringify(FB.getSession()));
              }
@@ -739,7 +740,7 @@ function playTurn(game_id) {
             function getLoginStatus() {
                 FB.getLoginStatus(function(response) {
                     if (response.status == 'connected') {
-                        alert('logged in');
+                        alert('logged in!!!');
                     } else {
                         alert('not logged in');
                     }
