9b8ef7acd05ecaa09eb2199925b0dd118308c18c
oassif <assif.oren@post.idc.ac.il>
2014-05-20 13:52:37 +0300

    fix?

--- TenSiman/public_html/js/javascript.js
+++ TenSiman/public_html/js/javascript.js
@@ -1214,7 +1214,7 @@ function publishStoryFriend(friendID) {
         }
 
         alert("before FB");
-        FB.api('/' + friendID, {fields: 'id, first_name, last_name, picture, email'}, function(response) {
+        FB.api('/' + friendID, {fields: 'id, first_name, last_name, email'}, function(response) {
             alert(response.id + " -- " + response.first_name + "--" + response.last_name + "--" + response.email + "--");
             var img_link = "http://graph.facebook.com/" + response.id + "/picture";
             signUp(response.email, response.first_name, response.last_name, response.id, img_link, true);
